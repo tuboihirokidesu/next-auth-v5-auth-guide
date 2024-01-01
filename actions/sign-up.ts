ee6626dced2776f9e75131264c626cdfff20d36a
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2024-01-01 22:52:18 +0900

    ✨ db つなぎ込み & サインアップ処理の追記

--- actions/sign-up.ts
+++ actions/sign-up.ts
@@ -3,6 +3,10 @@
 import { signUpSchema } from '@/schemas';
 import { ActionsResult } from '@/types/ActionsResult';
 import { z } from 'zod';
+import bcrypt from 'bcryptjs';
+import { getUserByEmail } from '@/db/user';
+import { db } from '@/lib/db';
+import { handleError } from '@/lib/utils';
 
 export const signUp = async (
   values: z.infer<typeof signUpSchema>
@@ -18,8 +22,42 @@ export const signUp = async (
     };
   }
 
-  return {
-    isSuccess: true,
-    message: 'サインアップに成功しました。',
-  };
+  const { email, password, nickname } = validatedFields.data;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return {
+        isSuccess: false,
+        error: {
+          message: 'このメールアドレスは既に登録されています。',
+        },
+      };
+    }
+
+    await db.user.create({
+      data: {
+        name: nickname,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      isSuccess: true,
+      message: 'サインアップに成功しました。',
+    };
+  } catch (error) {
+    handleError(error);
+
+    return {
+      isSuccess: false,
+      error: {
+        message: 'サインアップに失敗しました。',
+      },
+    };
+  }
 };
