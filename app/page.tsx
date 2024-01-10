03f786032f87112271e04fa319a477aa5270641c
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2024-01-10 14:52:16 +0900

    ✨ サインアウトボタン付与

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 async function MyAccountPage() {
@@ -6,7 +7,21 @@ async function MyAccountPage() {
 
   console.table(session);
 
-  return <div>Root Page</div>;
+  return (
+    <div>
+      <form
+        action={async () => {
+          'use server';
+
+          await signOut();
+        }}
+      >
+        <Button variant={'ghost'} type='submit'>
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default MyAccountPage;
